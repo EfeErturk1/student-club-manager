df396e9f169667dea17f0399218f248d9ab2c27d
Beste-Guney <63497613+Beste-Guney@users.noreply.github.com>
2021-12-26 02:47:52 +0300

    singleton model hibernate

--- src/main/java/com/example/cs319project/model/Admin.java
+++ src/main/java/com/example/cs319project/model/Admin.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Subselect;
 
 import javax.persistence.*;
 import java.util.Set;
@@ -16,6 +17,7 @@ import java.util.Set;
 @AllArgsConstructor
 @SuperBuilder
 @Entity
+@Subselect("select 'admin' as name")
 public class Admin {
     @Id
     public int id;
